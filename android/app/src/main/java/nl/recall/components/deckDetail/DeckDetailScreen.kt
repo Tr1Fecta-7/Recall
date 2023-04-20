8cc00de5cd3ce331c68239dcb8c4990b266455bd
BasPeters <bas.peters@hva.nl>
2023-04-20 20:58:02 +0200

    screen done

--- android/app/src/main/java/nl/recall/components/deckDetail/DeckDetailScreen.kt
+++ android/app/src/main/java/nl/recall/components/deckDetail/DeckDetailScreen.kt
@@ -12,24 +12,33 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,6 +46,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.DefaultTintColor
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -54,14 +64,19 @@ import nl.recall.theme.AppTheme
 
 @RootNavGraph(start = true)
 @Destination
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun DeckDetailScreen() {
+    val list: List<String> = listOf("boe", "bah", "", "")
+
+
     Scaffold(
         topBar = {
             TopAppBar(
-                modifier = Modifier.background(AppTheme.neutral50),
-
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = AppTheme.white
+                ),
                 title = {
                     Text(text = "title")
                 },
@@ -70,8 +85,10 @@ fun DeckDetailScreen() {
                         Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "go back")
                     }
                 },
-                actions = IconButton(onClick = { /*TODO*/ }) {
-                    Icon(painter = painterResource(id = Icons.Default.T), contentDescription = )
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.MoreVert, contentDescription = "")
+                    }
                 }
 
             )
@@ -81,6 +98,7 @@ fun DeckDetailScreen() {
                 modifier = Modifier
                     .padding(it)
                     .fillMaxSize(),
+
             ) {
                 Column(
                     modifier = Modifier
@@ -94,7 +112,10 @@ fun DeckDetailScreen() {
                         border = BorderStroke(1.dp, AppTheme.neutral400),
                         colors = CardDefaults.cardColors(
                             containerColor = AppTheme.white
-                        )
+                        ),
+                        onClick = {
+                            //To do
+                        }
                     ) {
                         Row(
                             verticalAlignment = Alignment.CenterVertically,
@@ -149,7 +170,7 @@ fun DeckDetailScreen() {
                         singleLine = true,
                         trailingIcon = {
                             if (false) {
-                                IconButton(onClick = {  }) {
+                                IconButton(onClick = { }) {
                                     Icon(
                                         imageVector = Icons.Outlined.Close,
                                         contentDescription = "clear textfield"
@@ -159,12 +180,56 @@ fun DeckDetailScreen() {
                                 IconButton(
                                     modifier = Modifier.padding(end = 6.dp),
                                     onClick = { /*TODO*/ }) {
-                                    Icon(imageVector = Icons.Default.Search, contentDescription = "search")
+                                    Icon(
+                                        imageVector = Icons.Default.Search,
+                                        contentDescription = "search"
+                                    )
                                 }
                             }
                         }
                     )
-
+                LazyColumn(
+                    modifier = Modifier.padding(top = 10.dp)
+                ) {
+                    items(
+                        items = list,
+                        itemContent = {
+                            Card(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(AppTheme.white)
+                                    .padding(top = 2.dp),
+                                border = BorderStroke(1.dp, AppTheme.neutral400),
+                                colors = CardDefaults.cardColors(
+                                    containerColor = AppTheme.white
+                                ),
+                                onClick = {
+                                    //To do
+                                }
+                            ) {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.padding(10.dp)
+                                ) {
+                                    Column(
+                                        modifier = Modifier
+                                            .padding(start = 10.dp)
+                                            .width(250.dp)
+                                    ) {
+                                        Text(
+                                            text = stringResource(id = R.string.start_learning_text)
+                                            )
+                                    }
+                                    Image(
+                                        modifier = Modifier.size(15.dp),
+                                        painter = painterResource(id = R.drawable.navigate_next),
+                                        contentDescription = "open card"
+                                    )
+                                }
+                            }
+                        }
+                    )
+                }
                 }
             }
         },
