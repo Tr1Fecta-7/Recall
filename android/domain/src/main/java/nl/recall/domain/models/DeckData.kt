e0ae45cc4183dc6168a2ae6f787694ca04bc2d1e
Tom de Waardt <tom.de.waardt@hva.nl>
2023-04-21 19:41:25 +0200

    changed some paramaters to var

--- android/domain/src/main/java/nl/recall/domain/models/DeckData.kt
+++ android/domain/src/main/java/nl/recall/domain/models/DeckData.kt
@@ -3,6 +3,6 @@ package nl.recall.domain.models
 data class DeckData(
     val title: String,
     val cards: Set<Card> = HashSet(),
-    val backgroundColor: String = "#DE2910",
-    val emoji: String = "\uD83E\uDDE0"
+    var backgroundColor: String = "#DE2910",
+    var emoji: String = "\uD83E\uDDE0"
 )
