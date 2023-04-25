1771728f5e75ceecc12e82f0364403cc814b1224
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2023-04-25 19:04:42 +0200

    rewrote deck preview to work with new deck data

--- android/app/src/main/java/nl/recall/components/deck/DeckPreview.kt
+++ android/app/src/main/java/nl/recall/components/deck/DeckPreview.kt
@@ -26,12 +26,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.toColorInt
 import nl.recall.R
+import nl.recall.domain.deck.model.Deck
 import nl.recall.domain.models.DeckPreviewData
 import nl.recall.theme.AndroidAppTheme
 import nl.recall.theme.AppTheme
+import java.util.Date
 
 @Composable
-fun DeckPreview(deck: DeckPreviewData, onClick: () -> Unit) {
+fun DeckPreview(deck: Deck, cardCount: Int, onClick: () -> Unit) {
     Card(
         onClick = { onClick() },
         shape = RoundedCornerShape(18.dp),
@@ -57,9 +59,9 @@ fun DeckPreview(deck: DeckPreviewData, onClick: () -> Unit) {
                     modifier = Modifier
                         .size(40.dp)
                         .clip(CircleShape)
-                        .background(Color(deck.backgroundColor.toColorInt()))
+                        .background(Color(deck.background_color.toColorInt()))
                 ) {
-                    Text(deck.emoji)
+                    Text(deck.icon)
                 }
 
                 Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
@@ -70,7 +72,7 @@ fun DeckPreview(deck: DeckPreviewData, onClick: () -> Unit) {
                         fontWeight = FontWeight.Bold
                     )
                     Text(
-                        stringResource(id = R.string.deck_card_count).format(deck.cardCount),
+                        stringResource(id = R.string.deck_card_count).format(cardCount),
                         style = MaterialTheme.typography.bodyMedium,
                         color = AppTheme.neutral400
                     )
@@ -88,16 +90,16 @@ fun DeckPreview(deck: DeckPreviewData, onClick: () -> Unit) {
 @Preview
 @Composable
 fun Preview() {
-    val deck = DeckPreviewData(
+    val deck = Deck(
+        id = 0,
         title = "Mandarin HSK 1",
-        cardCount = 512,
-        backgroundColor = "#FFEAEA",
-        emoji = "🇨🇳",
+        creationDate = Date(),
+        icon = "🇨🇳",
+        background_color = "#FFEAEA"
     )
+    val cardCount = 256
 
     AndroidAppTheme {
-        DeckPreview(deck, onClick = {
-
-        })
+        DeckPreview(deck, cardCount, onClick = {})
     }
 }
