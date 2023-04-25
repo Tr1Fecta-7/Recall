12e7273cf7f4fbbf69863c11ce430006c88c4471
Tom de Waardt <tom.de.waardt@hva.nl>
2023-04-25 13:20:08 +0200

    added viewmodel

---
+++ android/presentation/src/main/java/nl/recall/presentation/createDeck/model/CreateDeckViewModelArgs.kt
@@ -0,0 +1,7 @@
+package nl.recall.presentation.createDeck.model
+
+class CreateDeckViewModelArgs(id: Long, title: String, background_color: String){
+    val id = id
+    val title = title
+    val background_color = background_color
+}
