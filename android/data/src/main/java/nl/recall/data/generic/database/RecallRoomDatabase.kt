a8650ed64f7fa578636cba328344b9903c9f5231
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2023-04-26 21:04:56 +0200

    added room database migration fallback

--- android/data/src/main/java/nl/recall/data/generic/database/RecallRoomDatabase.kt
+++ android/data/src/main/java/nl/recall/data/generic/database/RecallRoomDatabase.kt
@@ -10,7 +10,7 @@ import nl.recall.data.deck.models.DeckEntity
 import nl.recall.data.deck.storage.CardDao
 import nl.recall.data.deck.storage.DeckDao
 
-@Database(entities = [DeckEntity::class, CardEntity::class], version = 3, exportSchema = false)
+@Database(entities = [DeckEntity::class, CardEntity::class], version = 4, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class RecallRoomDatabase : RoomDatabase() {
 
@@ -22,7 +22,9 @@ abstract class RecallRoomDatabase : RoomDatabase() {
         fun getDatabase(context: Context): RecallRoomDatabase {
             return Room.databaseBuilder(
                 context.applicationContext, RecallRoomDatabase::class.java, DATABASE_NAME
-            ).build()
+            )
+                .fallbackToDestructiveMigration()
+                .build()
         }
     }
 }
