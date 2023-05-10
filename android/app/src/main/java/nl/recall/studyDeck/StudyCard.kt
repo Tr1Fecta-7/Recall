5e4fa7a90fee86497e258aef6bfc9d537ac3d650
BasPeters <bas.peters@hva.nl>
2023-05-10 14:11:49 +0200

    update card rotation

--- android/app/src/main/java/nl/recall/studyDeck/StudyCard.kt
+++ android/app/src/main/java/nl/recall/studyDeck/StudyCard.kt
@@ -1,82 +1,43 @@
 package nl.recall.components.card
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.SizeTransform
-import androidx.compose.animation.core.CubicBezierEasing
-import androidx.compose.animation.core.animate
+
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.animation.with
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.alexstyl.swipeablecard.Direction
 import com.alexstyl.swipeablecard.ExperimentalSwipeableCardApi
 import com.alexstyl.swipeablecard.rememberSwipeableCardState
 import com.alexstyl.swipeablecard.swipableCard
 import nl.recall.R
 import nl.recall.domain.deck.model.DeckWithCards
 import nl.recall.presentation.studyDeck.model.CardFace
-import nl.recall.theme.AppTheme
 
 @OptIn(ExperimentalSwipeableCardApi::class)
 @Composable
 fun StudyCard(deckWithCards: DeckWithCards) {
     val progressStep = (1.0000f / deckWithCards.cards.size)
     val state = rememberSwipeableCardState()
+    var iterator by remember {
+        mutableStateOf(deckWithCards.cards.size.dec())
+    }
     var progress by remember { mutableStateOf(0.0000f) }
     val animatedProgress = animateFloatAsState(
         targetValue = progress,
@@ -111,15 +72,15 @@ fun StudyCard(deckWithCards: DeckWithCards) {
                 .padding(24.dp)
                 .fillMaxSize()
         ) {
-            cards.forEach { (card, cardState) ->
+            cards.forEachIndexed() { index, (card, cardState) ->
                 var cardFace by remember {
                     mutableStateOf(CardFace.Front)
                 }
-
-                if (cardState.swipedDirection == null) {
+//                        && (index == iterator || index ==(iterator-1))
+                if (cardState.swipedDirection == null && (index >= iterator-1) ) {
                     FlipCard(
                         cardFace = cardFace,
-                        onClick = { cardFace = cardFace.next },
+                        onClick = { cardFace = CardFace.Back },
                         modifierFront = Modifier,
                         modifierBack = Modifier
                             .swipableCard(
@@ -127,6 +88,7 @@ fun StudyCard(deckWithCards: DeckWithCards) {
                                 onSwiped = { direction ->
                                     println("The card was swiped to $direction")
                                     progress += progressStep
+                                    iterator--
                                 },
                                 onSwipeCancel = {
                                     println("The swiping was cancelled")
