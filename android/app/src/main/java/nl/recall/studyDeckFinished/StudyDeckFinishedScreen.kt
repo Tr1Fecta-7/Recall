8fca493b14a828a68e8a8ced723a12fb11f4d323
Tom de Waardt <tom.de.waardt@hva.nl>
2023-05-16 15:14:54 +0200

    frontend for study deck completion screen

---
+++ android/app/src/main/java/nl/recall/studyDeckFinished/StudyDeckFinishedScreen.kt
@@ -0,0 +1,118 @@
+package nl.recall.studyDeckFinished
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import nl.recall.R
+import nl.recall.theme.AppTheme
+import nl.recall.theme.md_theme_light_primary
+
+@Composable
+fun StudyDeckFinishedScreen(navigator: DestinationsNavigator){
+    val context = LocalContext.current
+    checkStreak(context)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .width(150.dp)
+                .height(150.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Column(modifier = Modifier
+                .clip(RoundedCornerShape(75.dp))
+                .background(AppTheme.primary100)
+                .width(150.dp)
+                .height(150.dp)
+                .border(
+                    width = 1.dp,
+                    color = AppTheme.primary300,
+                    shape = RoundedCornerShape(75.dp)
+                )
+                .padding(bottom = 15.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "\uD83C\uDFC6", fontSize = 100.sp)
+            }
+        }
+        Text(
+            text = stringResource(id = R.string.completed),
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.good_job),
+            fontSize = 16.sp,
+            color = AppTheme.neutral400,
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
+        Column(
+            modifier = Modifier
+                .width(200.dp)
+                .height(75.dp)
+                .clip(RoundedCornerShape(15.dp))
+                .background(AppTheme.primary100)
+                .border(
+                    width = 1.dp,
+                    color = AppTheme.primary300,
+                    shape = RoundedCornerShape(15.dp)
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = String.format(stringResource(id = R.string.streak_number), 1))
+            Text(text = stringResource(id = R.string.day_streak))
+        }
+        Column(modifier = Modifier.padding(20.dp)) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(40.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = md_theme_light_primary)
+            ){
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "back", tint = AppTheme.white)
+                Text(text = stringResource(id = R.string.back_to_overview), color = AppTheme.white, modifier = Modifier.padding(start = 10.dp))
+            }
+        }
+    }
+}
+
+@Composable
+private fun checkStreak(context: Context){
+    val sharedPreferences = context.applicationContext.getSharedPreferences(stringResource(id = R.string.shared_preferences_streak_file), Context.MODE_PRIVATE)
+//    val edit = sharedPreferences.edit()
+//    edit.putString(stringResource(id = R.string.shared_preferences_start_streak_day))
+}
