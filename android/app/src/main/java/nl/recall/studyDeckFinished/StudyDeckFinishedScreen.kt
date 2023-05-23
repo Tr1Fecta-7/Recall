a60c644765b537c8030a8c1bec36cf67cdabef1a
Tom de Waardt <tom.de.waardt@hva.nl>
2023-05-23 13:51:12 +0200

    added animation for streak

--- android/app/src/main/java/nl/recall/studyDeckFinished/StudyDeckFinishedScreen.kt
+++ android/app/src/main/java/nl/recall/studyDeckFinished/StudyDeckFinishedScreen.kt
@@ -1,9 +1,16 @@
 package nl.recall.studyDeckFinished
+
 import android.content.Context
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,6 +23,14 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,76 +41,110 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
 import nl.recall.R
 import nl.recall.theme.AppTheme
 import nl.recall.theme.md_theme_light_primary
 import java.util.Calendar
 import java.util.Calendar.DAY_OF_YEAR
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun StudyDeckFinishedScreen(navigator: DestinationsNavigator){
+fun StudyDeckFinishedScreen(navigator: DestinationsNavigator) {
     val context = LocalContext.current
-    val currentStreak: Int = checkStreak(context)
-
+    val oldStreak = context.applicationContext.getSharedPreferences(
+        stringResource(id = R.string.shared_preferences_streak_file),
+        0
+    ).getInt(
+        stringResource(id = R.string.shared_preferences_current_streak), 0
+    );
+    var currentStreak by remember { mutableStateOf(oldStreak) }
+    LaunchedEffect(key1 = true) {
+        delay(1000)
+        currentStreak = checkStreak(context = context)
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(top = 50.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ) {
+        val widthSize = 150.dp
         Column(
-            modifier = Modifier
-                .width(150.dp)
-                .height(150.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            modifier = Modifier.weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(modifier = Modifier
-                .clip(RoundedCornerShape(75.dp))
-                .background(AppTheme.primary100)
-                .width(150.dp)
-                .height(150.dp)
-                .border(
-                    width = 1.dp,
-                    color = AppTheme.primary300,
-                    shape = RoundedCornerShape(75.dp)
-                )
-                .padding(bottom = 15.dp),
+            Column(
+                modifier = Modifier
+                    .width(widthSize)
+                    .height(widthSize),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                Text(text = "\uD83C\uDFC6", fontSize = 100.sp)
+                Column(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(widthSize / 2))
+                        .background(AppTheme.primary100)
+                        .width(widthSize)
+                        .height(widthSize)
+                        .border(
+                            width = 1.dp,
+                            color = AppTheme.primary300,
+                            shape = RoundedCornerShape(widthSize / 2)
+                        )
+                        .padding(bottom = 15.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(text = "\uD83C\uDFC6", fontSize = 90.sp)
+                }
             }
-        }
-        Text(
-            text = stringResource(id = R.string.completed),
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            modifier = Modifier.padding(bottom = 10.dp)
-        )
-        Text(
-            text = stringResource(id = R.string.good_job),
-            fontSize = 16.sp,
-            color = AppTheme.neutral400,
-            modifier = Modifier.padding(bottom = 10.dp)
-        )
-        Column(
-            modifier = Modifier
-                .width(200.dp)
-                .height(75.dp)
-                .clip(RoundedCornerShape(15.dp))
-                .background(AppTheme.primary100)
-                .border(
-                    width = 1.dp,
-                    color = AppTheme.primary300,
-                    shape = RoundedCornerShape(15.dp)
-                ),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(text = String.format(stringResource(id = R.string.streak_number), currentStreak))
-            Text(text = stringResource(id = R.string.day_streak))
+            Column(
+                modifier = Modifier.padding(top = 10.dp, bottom = 50.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.completed),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(bottom = 10.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.good_job),
+                    fontSize = 16.sp,
+                    color = AppTheme.neutral400,
+                    modifier = Modifier.padding(bottom = 10.dp)
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(75.dp)
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(AppTheme.primary100)
+                    .border(
+                        width = 1.dp,
+                        color = AppTheme.primary300,
+                        shape = RoundedCornerShape(15.dp)
+                    )
+                    .padding(top = 10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Row() {
+                    Text(text = stringResource(id = R.string.fire_emoji))
+                    AnimatedContent(
+                        targetState = currentStreak,
+                        transitionSpec = {
+                            slideInVertically { -it } with slideOutVertically { it }
+                        }) {
+                        Text(text = it.toString())
+                    }
+                }
+                Text(text = stringResource(id = R.string.day_streak))
+            }
         }
         Column(modifier = Modifier.padding(20.dp)) {
             Button(
@@ -104,31 +153,40 @@ fun StudyDeckFinishedScreen(navigator: DestinationsNavigator){
                     .width(300.dp)
                     .height(40.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = md_theme_light_primary)
-            ){
-                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "back", tint = AppTheme.white)
-                Text(text = stringResource(id = R.string.back_to_overview), color = AppTheme.white, modifier = Modifier.padding(start = 10.dp))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "back",
+                    tint = AppTheme.white
+                )
+                Text(
+                    text = stringResource(id = R.string.back_to_overview),
+                    color = AppTheme.white,
+                    modifier = Modifier.padding(start = 10.dp)
+                )
             }
         }
     }
 }
 
-@Composable
-private fun checkStreak(context: Context): Int{
-    val sharedPreferences = context.applicationContext.getSharedPreferences(stringResource(id = R.string.shared_preferences_streak_file), Context.MODE_PRIVATE)
+
+private fun checkStreak(context: Context): Int {
+    val sharedPreferences =
+        context.applicationContext.getSharedPreferences("streakFile", Context.MODE_PRIVATE)
     val edit = sharedPreferences.edit()
     val calendar = Calendar.getInstance()
     val currentDay: Int = calendar.get(DAY_OF_YEAR)
-    val lastStreakDay = sharedPreferences.getInt(stringResource(id = R.string.shared_preferences_last_streak_day), 0)
-    val currentStreakDay = sharedPreferences.getInt(stringResource(id = R.string.shared_preferences_current_streak), 0)
-    if(currentStreakDay == currentDay) return currentStreakDay
+    val lastStreakDay = sharedPreferences.getInt("lastStreakDay", 0)
+    val currentStreakDay = sharedPreferences.getInt("currentStreak", 0)
+    if (currentStreakDay == currentDay) return currentStreakDay
 
-    val streak =  if (lastStreakDay == currentDay - 1){
-        edit.putInt(stringResource( id= R.string.shared_preferences_current_streak), currentStreakDay + 1)
-        edit.putInt(stringResource(id = R.string.shared_preferences_last_streak_day), currentDay)
-        currentStreakDay+1
+    val streak = if (lastStreakDay == currentDay - 1) {
+        edit.putInt("currentStreak", currentStreakDay + 1)
+        edit.putInt("lastStreakDay", currentDay)
+        currentStreakDay + 1
     } else {
-        edit.putInt(stringResource(id = R.string.shared_preferences_last_streak_day), currentDay)
-        edit.putInt(stringResource(id = R.string.shared_preferences_current_streak), 1)
+        edit.putInt("lastStreakDay", currentDay)
+        edit.putInt("currentStreak", 1)
         1
     }
     edit.apply()
