1f21d399616ad29560920fe55bcc74b9473ab940
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2023-05-23 12:21:27 +0200

    added community deck overview view model

--- android/presentation/src/main/java/nl/recall/presentation/communityDeckOverview/CommunityDeckViewModel.kt
+++ android/presentation/src/main/java/nl/recall/presentation/communityDeckOverview/CommunityDeckViewModel.kt
@@ -1,4 +1,60 @@
 package nl.recall.presentation.communityDeckOverview
 
-class CommunityDeckViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import nl.recall.domain.deck.model.Deck
+import nl.recall.presentation.uiState.UIState
+import org.koin.android.annotation.KoinViewModel
+
+@KoinViewModel
+class CommunityDeckViewModel(
+
+): ViewModel() {
+	private val _state = MutableStateFlow(UIState.LOADING)
+	val state: StateFlow<UIState> by lazy {
+		_state.asStateFlow()
+	}
+
+	private val _decks = MutableStateFlow<Map<Deck, Int>>(emptyMap())
+	val decks: StateFlow<Map<Deck, Int>> by lazy {
+		fetchDecks()
+		_decks.asStateFlow()
+	}
+
+	private fun fetchDecks() {
+		_state.value = UIState.LOADING
+
+		viewModelScope.launch(Dispatchers.IO) {
+			// _decks.value = getDecksWithCardCount()
+
+			if (_decks.value.isEmpty()) {
+				_state.value = UIState.EMPTY
+			} else {
+				_state.value = UIState.NORMAL
+			}
+		}
+	}
+
+	fun searchDecks(title: String) {
+		_state.value = UIState.LOADING
+
+		viewModelScope.launch(Dispatchers.IO) {
+			// if (title.isEmpty()) {
+			// 	_decks.value = getDecksWithCardCount()
+			// } else {
+			// 	_decks.value = searchDecksWithCardCount(title)
+			// }
+
+			if (_decks.value.isEmpty()) {
+				_state.value = UIState.EMPTY
+			} else {
+				_state.value = UIState.NORMAL
+			}
+		}
+	}
 }
