c6609ee1fcb0b3c0a426553728f34c8d09860007
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2023-05-27 13:43:05 +0200

    changed max width for the image message

--- android/app/src/main/java/nl/recall/components/ImageMessage.kt
+++ android/app/src/main/java/nl/recall/components/ImageMessage.kt
@@ -3,7 +3,6 @@ package nl.recall.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,28 +11,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun ImageMessage(
-    painter: Painter,
-    text: String,
+	painter: Painter,
+	text: String,
 ) {
-    Column(
-        Modifier.fillMaxSize()
-    ) {
-        Column(
+	Column(
+		Modifier.fillMaxSize()
+	) {
+		Column(
             Modifier
                 .fillMaxWidth()
                 .padding(top = 60.dp),
-            verticalArrangement = Arrangement.spacedBy(20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painter,
-                contentDescription = text
-            )
-            Text(text = text)
-        }
-    }
+			verticalArrangement = Arrangement.spacedBy(20.dp),
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
+			Image(
+				modifier = Modifier.fillMaxWidth(0.8f),
+				painter = painter,
+				contentDescription = text
+			)
+			Text(
+				text = text,
+				textAlign = TextAlign.Center
+			)
+		}
+	}
 }
