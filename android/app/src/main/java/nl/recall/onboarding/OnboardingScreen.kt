c6e7ec113072ae3744979579b5252d7bec171e05
Jalil Faquiri <jalil.faquiri@hva.nl>
2023-05-30 01:56:16 +0200

    Implemented custom page indicator and fixed image sizing

--- android/app/src/main/java/nl/recall/onboarding/OnboardingScreen.kt
+++ android/app/src/main/java/nl/recall/onboarding/OnboardingScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,8 +17,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -56,23 +61,63 @@ fun MainContent(navController: NavController, navigator: DestinationsNavigator, 
 
     Box(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
     ) {
         HorizontalPager(pageCount = items.size, state = pagerState) { page ->
             OnboardingPage(items = items[page])
         }
 
-        HorizontalPagerIndicator(
-            modifier = Modifier.padding(16.dp),
+        CustomPagerIndicator(
             pagerState = pagerState,
             pageCount = items.size,
-            indicatorWidth = 10.dp,
+            activeColor = AppTheme.primary400, // Color for active (current) dot
+            inactiveColor = AppTheme.neutral300 // Color for inactive (passed) dots
         )
     }
 
 
 }
 
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun CustomPagerIndicator(
+    pagerState: PagerState,
+    pageCount: Int,
+    activeColor: Color,
+    inactiveColor: Color
+) {
+    Row(
+        modifier = Modifier.padding(16.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        repeat(pageCount) { pageIndex ->
+            val dotShape = if (pageIndex == pagerState.currentPage) {
+                RoundedCornerShape(10.dp)
+            } else {
+                CircleShape
+            }
+
+            val dotColor = if (pageIndex == pagerState.currentPage) {
+                activeColor
+            } else {
+                inactiveColor
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(width = if (pageIndex == pagerState.currentPage) 50.dp else 10.dp, height = 10.dp)
+                    .background(color = dotColor, shape = dotShape)
+                    .padding(horizontal = 4.dp)
+            )
+
+            if (pageIndex < pageCount - 1) {
+                Spacer(modifier = Modifier.width(4.dp))
+            }
+        }
+    }
+}
 
 @Composable
 fun OnboardingPage(items: OnboardingItems) {
@@ -80,26 +125,25 @@ fun OnboardingPage(items: OnboardingItems) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 50.dp, start = 16.dp, end = 16.dp)
+            .padding(top = 125.dp, start = 16.dp, end = 16.dp)
     ) {
         val painter = painterResource(id = items.image)
         Image(
-            modifier = Modifier.size(450.dp),
+            modifier = Modifier
+                .height(300.dp)
+                .fillMaxWidth(),
             painter = painter,
             contentDescription = "",
         )
 
-        val yPadding = (-30).dp
         Text(
             text = stringResource(id = items.title),
-            modifier = Modifier.offset(y = yPadding),
             fontSize = 36.sp,
             fontWeight = FontWeight.SemiBold
         )
         Text(
             text = stringResource(id = items.desc),
             modifier = Modifier
-                .offset(y = yPadding)
                 .padding(start = 8.dp, end = 8.dp),
             color = AppTheme.neutral400,
             fontSize = 16.sp,
