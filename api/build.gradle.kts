a63caf7fa4d0d0969afd79fed51fcc1e9f1ed704
Jalil Faquiri <jalil.faquiri@hva.nl>
2023-06-02 14:40:22 +0200

    Try fix build.gradle.kts again^2

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -33,6 +33,17 @@ tasks.withType<Jar> {
 	manifest {
 		attributes["Main-Class"] = "com.recall.api.ApiApplicationKt"
 	}
+
+	// To avoid the duplicate handling strategy error
+	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+	// To add all of the dependencies
+	from(sourceSets.main.get().output)
+
+	dependsOn(configurations.runtimeClasspath)
+	from({
+		configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+	})
 }
 
 tasks.withType<KotlinCompile> {
