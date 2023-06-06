7fe0e2eb3da23ff6e8b0746749dc04a14afd006b
Jalil Faquiri <jalil.faquiri@hva.nl>
2023-06-06 15:54:20 +0200

    Modify function to not suspend in repo

--- android/domain/src/main/java/nl/recall/domain/repositories/OnboardingRepository.kt
+++ android/domain/src/main/java/nl/recall/domain/repositories/OnboardingRepository.kt
@@ -1,6 +1,6 @@
 package nl.recall.domain.repositories
 
 interface OnboardingRepository {
-    suspend fun getOnboardingCompleted() : Boolean
+    fun getOnboardingCompleted() : Boolean
     suspend fun setOnboardingCompleted(completed: Boolean)
 }
